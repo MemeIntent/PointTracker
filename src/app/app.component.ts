43c9bb75025024e4bd8197d0ba2ebfbfa3dd1e1c
MemeIntent <47615410+MemeIntent@users.noreply.github.com>
2022-08-14 19:13:42 -0400

    Changes by ag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { AngularFireAuth } from "@angular/fire/compat/auth";
 import firebase from 'firebase/compat/app';
 
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
