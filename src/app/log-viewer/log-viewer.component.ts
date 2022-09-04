94c599034d16dcaca755c2849a437fadf8ac8dbb
Aiden Gilmore <91899915+DominantDuck@users.noreply.github.com>
2022-09-04 09:36:07 -0400

    update errors

--- src/app/log-viewer/log-viewer.component.ts
+++ src/app/log-viewer/log-viewer.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import firebase from 'firebase/compat/app';
 import {AngularFirestore} from "@angular/fire/compat/firestore";
+import { getDatabase } from "firebase/database";
+import {collection} from "@angular/fire/firestore";
+
+const database = getDatabase();
+
 
 @Component({
   selector: 'app-log-viewer',
@@ -9,9 +14,48 @@ import {AngularFirestore} from "@angular/fire/compat/firestore";
 })
 export class LogViewerComponent implements OnInit {
 
+  // houseVar = "a";
+  // pointsVar  = 0
+  // reasonVar = "a"
+  // studentVar = "a"
+  // teacherVar = "a"
+  //
+  //
+  // refresh() {
+  //   let collection = this.db.collection('logs');
+  //
+  //   let houseDoc = collection.doc("house")
+  //   let pointsDoc = collection.doc("points")
+  //   let reasonDoc = collection.doc("reason")
+  //   let studentDoc = collection.doc("student")
+  //   let teacherDoc = collection.doc("teacher")
+  //
+  //   houseDoc.get().subscribe(ref => {
+  //     const doc: any = ref.data();
+  //     this.houseVar = doc.house
+  //   })
+  //   pointsDoc.get().subscribe(ref => {
+  //     const doc: any = ref.data();
+  //     this.pointsVar = doc.points
+  //   })
+  //   reasonDoc.get().subscribe(ref => {
+  //     const doc: any = ref.data();
+  //     this.reasonVar = doc.reason
+  //   })
+  //   studentDoc.get().subscribe(ref => {
+  //     const doc: any = ref.data();
+  //     this.studentVar = doc.student
+  //   })
+  //   teacherDoc.get().subscribe(ref => {
+  //     const doc: any = ref.data();
+  //     this.teacherVar = doc.teacher
+  //   })
+  // }
+
   constructor(private db: AngularFirestore) { }
 
   ngOnInit(): void {
+    // this.refresh()
   }
 
 }
